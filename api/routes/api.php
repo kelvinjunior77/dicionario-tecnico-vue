ff03bcb40775a701583e4c5f68647511d143204d
Kelvin Junior <105466232+kelvinjunior99@users.noreply.github.com>
2024-11-27 23:36:23 +0100

    Update api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-
-Route::get('dicionario', [Api])
 */
 
 Route::get('/dicionario', [ApiController::class, 'index']);
